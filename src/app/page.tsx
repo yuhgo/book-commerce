7d16468d486b9e7ba05c5ac06405be5fa54b48c0
ゆうご <yuhgo090764@gmail.com>
2024-01-29 19:46:24 +0900

    Remove unused code in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import type { ReactNode } from "react";
-
 export default function TopPage() {
 	return <div>hoge</div>;
 }
-
-export const Container = ({ children }: { children: ReactNode }) => {
-	return <div>{children}</div>;
-};
