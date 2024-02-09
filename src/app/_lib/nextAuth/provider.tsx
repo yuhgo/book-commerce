48f8aed138bdd234985c6e79c68d857ee2dd255c
ゆうご <yuhgo090764@gmail.com>
2024-02-09 14:14:03 +0900

    Update NextAuthProvider to include basePath in SessionProvider

--- src/app/_lib/nextAuth/provider.tsx
+++ src/app/_lib/nextAuth/provider.tsx
@@ -4,5 +4,5 @@ import { SessionProvider } from "next-auth/react";
 import type { FC, PropsWithChildren } from "react";
 
 export const NextAuthProvider: FC<PropsWithChildren> = ({ children }) => {
-	return <SessionProvider>{children}</SessionProvider>;
+	return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 };
